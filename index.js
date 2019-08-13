26391652e4aaf1749a4b247a3be698b5bc704d73
Tyson Rai <jovensrai@gmail.com>
2019-08-12 18:11:21 -0700

    improved logic to create a and b variables which store 2 operands at a time and an operator variable which stores the current operator and then a function that operates the expression

--- index.js
+++ index.js
@@ -1,60 +1,75 @@
-// const calculator = {
-// 	displayValue: '0'
-// }
-
-
-
-
-function add(a, b) {
-	return a + b;
-}
-
-function subtract(a, b) {
-	return a - b
-}
-
-function multiply(a, b) {
-	return a * b;
-}
-
-function divide(a, b) {
-	return a / b;
-}
-
-
-let displayBottomValue = "";
-let equation = "";
- const displayBottom = document.querySelector('#displayBottom');
-const displayTop = document.querySelector('#displayTop');
-
 const buttons = document.querySelectorAll('button');
+let displayBottom = document.querySelector('#displayBottom');
+let displayTop = document.querySelector('#displayTop');
+const operators = ['+', '-', '*', '/'];
+let operandFlag = false;
+let firstOperator = '';
+let secondOperator = '';
+let displayValue = "";
+let a = '';
+let b = '';
 
-buttons.forEach(btn => {
+
+function add(a, b) {return parseInt(a) + parseInt(b)};
+function subtract(a, b) {return parseInt(a) - parseInt(b)};
+function multiply(a, b) {return parseInt(a) * parseInt(b)};
+function divide(a, b) {return parseInt(a) / parseInt(b)};
+function operate(a, b, operator) {
+	switch (operator) {
+		case '+':
+			displayValue = add(a, b);
+			display(displayValue)
+			a = displayValue;
+			b = '';
+			console.log('a', a)
+			console.log('b', b)
+			firstOperator = secondOperator;
+			secondOperator = '';
+			displayValue += firstOperator
+			console.log('firstOperator', firstOperator)
+			console.log('secondOperator', secondOperator)
+			console.log('displayValue', displayValue)
+			break;
+		default:
+			console.log('not a plus')
+	}
+};
+function display(str) {
+	displayBottom.innerHTML = str;
+}
+
+buttons.forEach(function(btn) {
 	btn.addEventListener('click', (e) => {
-		if(btn.textContent == "AC") {
-			equation = "";
-			displayTop.innerHTML = "";
-			displayBottom.innerHTML = "";
-			displayBottomValue = "";
-		} else if (btn.textContent == "=") {
-			operate();
-			displayTop.innerHTML += equation;
+		let text = btn.textContent;
+		if(parseInt(text) > -1 && parseInt(text) < 10) {
+			if(firstOperator !== '') {
+				b += text;
+				displayValue += text;
+				display(displayValue);
+			} else {
+				displayValue += text;
+				display(displayValue)
+			}
+		} else if(text === ".") {
+			console.log(text)
+		} else if(operators.indexOf(text) !== -1) {
+			if(firstOperator !== '') {
+				secondOperator = text;
+				operate(a, b, firstOperator)
+				// displayValue += text;
+			} else {
+				console.log('there is now one operator')
+				a = displayValue;
+				firstOperator = text;
+				displayValue += text;
+				display(displayValue)
+			}
+		} else if(text === "AC") {
+			console.log('Clear')
 		} else {
-			displayBottomValue += btn.textContent;
-			display(displayBottomValue)
+			console.log("operating")
 		}
 	})
 })
-
-function display(str) {
-	displayBottom.innerHTML += str
-	displayBottomValue = ""
-	equation += str
-}
-
-function operate() {
-	displayBottom.innerHTML = "";
-}
-
 
 
